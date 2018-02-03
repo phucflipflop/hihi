59a7e38269a9f482b6165797b4f061be5415a0d6
phucflipflop <phucflipflop@gmail.com>
2018-02-03 19:51:47 +0700

    ban cap nhat audition thang 10

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,11 @@ namespace hihi
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             Close();
         }
     }
